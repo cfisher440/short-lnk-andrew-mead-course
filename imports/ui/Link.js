8604ad7d3620509423df7f6fc656d977de5e0ed0
Charles Fisher <cfisher440@gmail.com>
2017-10-02 12:47:25 +0000

    section 6 lecture 56

---
+++ imports/ui/Link.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { browserHistory } from 'react-router';
+
+export default class Link extends React.Component {
+  onLogout() {
+    browserHistory.push('/');
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Links</h1>
+        <button onClick={this.onLogout.bind(this)}>Logout</button>
+      </div>
+    );
+  }
+}
---
+++ imports/ui/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Login to Short Lnk</h1>
+        
+        login form here
+        
+        <Link to="/signup">Have an account?</Link>
+      </div>
+    );
+    return <p>Login component here</p>
+  }
+}
